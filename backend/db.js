641a0c5c06b1d8320ee7c815964cfddf9ba9af76
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-07-03 03:37:29 +0530

    till task-4

--- backend/db.js
+++ backend/db.js
@@ -6,6 +6,35 @@ app.use(express.json());
 mongoose.connect(
     "mongodb+srv://harshitbudhraja0:MongoKaPass@cluster0.nruhe7l.mongodb.net/"
 );
-const User = new mongoose.Schema({
-
+const UserSchema = new mongoose.Schema({
+    usernmae: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50,
+    },
 });
+const User = mongoose.model("User", UserSchema);
+module.exports = {
+    User,
+};
