24fd005b76aab3f49481ddc2600912804d2cf762
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-07-11 03:16:57 +0530

    all changes completed

--- backend/middlewares/middleware.js
+++ backend/middlewares/middleware.js
@@ -22,10 +22,10 @@ const authMiddleware = (req, res, next) => {
     }
 };
 module.exports = { authMiddleware };
-
+/*
 const JWT_SECRET = require("../config");
 const jwt = require("jsonwebtoken");
-/*
+
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith("Bearer")) {
