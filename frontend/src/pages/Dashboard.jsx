5467bd442ea0729d3f6fafbadc6200f04a7cc135
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-07-11 01:36:30 +0530

    getting actual balance from the backend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,27 @@
+import axios from "axios";
 import { Appbar } from "../components/Appbar";
 import { Balance } from "../components/Balance";
 import { Users } from "../components/Users";
+import { useEffect, useState } from "react";
 
 export const Dashboard = () => {
+    const [balance, setBalance] = useState(0);
+    useEffect(() => {
+        const fetchBalance = async () => {
+            const res = axios("http://localhost:3000/api/v1/account/balance", {
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("token"),
+                },
+            });
+            setBalance(res.data.balance);
+        };
+        fetchBalance();
+    }, []);
     return (
         <div>
             <Appbar />
             <div className="m-8">
-                <Balance value={"10,000"} />
+                <Balance value={balance} />
                 <Users />
             </div>
         </div>
